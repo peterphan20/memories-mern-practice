a6054096612f994333de952d11ea711441080388
Peter Phan <peterphan0422@gmail.com>
2021-06-15 22:17:05 -0500

    feat: CRUB functionality implemented

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -5,13 +6,12 @@ import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 
-const CONNECTION_STRING =
-	"mongodb+srv://pephan:Aggies0422@cluster0.hgyki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 // Create express server
 const app = express();
+dotenv.config();
 // Declare port for express server, either server env or 5000
 const PORT = process.env.PORT || 5000;
+
 // Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
 
 // Mongoose helps us connect to MongoDB
 mongoose
-	.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 	.catch((error) => console.error(error));
 mongoose.set("useFindAndModify", false);
